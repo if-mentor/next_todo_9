4db485818f85112369bfaa4a0d8a4fb90a888a6c
takahiro14 <takahiro14@yahoo.co.jp>
2023-02-08 01:07:50 +0900

    #9 refactor: TodoNewページデザイン変更/共通ヘッダに変更

--- src/pages/todonew.tsx
+++ src/pages/todonew.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Layout from "@/components/Layout";
 
 //Chakra UI
 import {
@@ -18,57 +19,65 @@ import {
 const todonew = () => {
   return (
     <div>
-      <Box
-        mb={3}
-        w="100%"
-        h="50px"
-        bg="green.300"
-        pl={10}
-        pt={1}
-        fontSize="1.8em"
-      >
-        TODOあああ
-      </Box>
-      <Box mr={10} ml={10}>
-        <FormControl>
-          <Flex>
-            <Box w={"100%"}>
-              <FormLabel htmlFor="newtodo">NEW TODO</FormLabel>
-            </Box>
-            <Box textAlign={"right"}>
+      <Layout>
+        <Box mr={10} ml={10}>
+          <FormControl>
+            <Flex mt={5}>
+              <Box w={"100%"}>
+                <FormLabel
+                  htmlFor="newtodo"
+                  fontWeight={"bold"}
+                  fontSize={"28px"}
+                >
+                  NEW TODO
+                </FormLabel>
+              </Box>
+              <Box textAlign={"right"}>
+                <Button
+                  bg="#95E3F4"
+                  type="button"
+                  borderRadius="full"
+                  variant="outline"
+                >
+                  Back
+                </Button>
+              </Box>
+            </Flex>
+            <Text mb={1} fontWeight={"bold"} fontSize={"24px"}>
+              TITLE
+            </Text>
+            <Input mb={5} type="text" defaultValue="Text" />
+            <Text mb={1} fontWeight={"bold"} fontSize={"24px"}>
+              DETAIL
+            </Text>
+            <Textarea mb={5} h="192px" defaultValue="Text" />
+            <Text mb={1} fontWeight={"bold"} fontSize={"24px"}>
+              PRIORITY
+            </Text>
+            <RadioGroup>
+              <Stack direction="row" fontSize={"24px"}>
+                <Radio value="1">High</Radio>
+                <Radio value="2">Middle</Radio>
+                <Radio value="3">Low</Radio>
+              </Stack>
+            </RadioGroup>
+            <Box mt={4} textAlign={"right"}>
+              {" "}
+              <Button mr={3} type="button" borderRadius="full" bg="red.100">
+                DRAFT
+              </Button>
               <Button
-                bg="green.200"
-                type="button"
+                bg="#40D1F1"
+                colorScheme="twitter"
+                type="submit"
                 borderRadius="full"
-                variant="outline"
               >
-                Back
+                CREATE
               </Button>
             </Box>
-          </Flex>
-          <Text mb={1}>TITLE</Text>
-          <Input mb={5} type="text" defaultValue="Text" />
-          <Text mb={1}>DETAIL</Text>
-          <Textarea id="message" mb={5} h="150px" defaultValue="Text" />
-          <Text mb={1}>PRIORITY</Text>
-          <RadioGroup>
-            <Stack direction="row">
-              <Radio value="1">High</Radio>
-              <Radio value="2">Middle</Radio>
-              <Radio value="3">Low</Radio>
-            </Stack>
-          </RadioGroup>
-          <Box mt={4} textAlign={"right"}>
-            {" "}
-            <Button mr={3} type="button" borderRadius="full" bg="red.100">
-              DRAFT
-            </Button>
-            <Button colorScheme="green" type="submit" borderRadius="full">
-              CREATE
-            </Button>
-          </Box>
-        </FormControl>
-      </Box>
+          </FormControl>
+        </Box>
+      </Layout>
     </div>
   );
 };
